def6374bc076e1e15f54eb184216832069e070e6
RizkyF11 <rizkyfebrian241@gmail.com>
2025-04-29 14:01:08 +0700

    pirst commit

---
+++ database/migrations/2025_04_29_055356_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function(Blueprint $table) {
+            $table->increments('id_user');
+            $table->string('nama_user',191);
+            $table->string('email',191);
+            $table->string('password', 191);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ database/migrations/2025_04_29_060042_orders.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id_orders');
+            $table->string('invoice', 191);
+            $table->unsignedBigInteger('customer_id'); // gunakan unsigned jika foreign key
+            $table->unsignedBigInteger('id_user');     // gunakan unsigned jika foreign key
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
