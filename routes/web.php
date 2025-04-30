c6c9bead93144ae8591adbf8a5c2aeacd58d3095
RizkyF11 <rizkyfebrian241@gmail.com>
2025-04-30 07:21:32 +0700

    templating

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LayananController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [HomeController::class, 'index'] );
